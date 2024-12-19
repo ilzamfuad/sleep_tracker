a31688d165d6358decd0fffb2cc7250d17d4f43f
ilzamfuady <muhammad.ilzam@wartek.belajar.id>
2024-12-19 14:49:26 +0700

    fix query get list sleep record

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -6,4 +6,9 @@ class SleepRecord < ApplicationRecord
   def self.get_record(user_id)
     where(user_id: user_id).order(created_at: :desc).limit(LIMIT_TWO_WEEK)
   end
+
+  def self.list_records(user_ids)
+    previous_week_start = Date.today.beginning_of_week - 1.week
+    where(user_id: user_ids).where(created_at: previous_week_start.beginning_of_day..Time.now).order(duration: :desc)
+  end
 end
